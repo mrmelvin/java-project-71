a9ad38a85d20f693c0226195b9d211095b41006d
mr.melvin <korobov.mike@gmail.com>
2022-08-24 23:08:46 +0500

    Add help for main method

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+//    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+//    boolean usageHelpRequested;
+//
+//    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+//    boolean versionInfoRequested;
+
+    public Integer call() throws Exception {
+//        if (usageHelpRequested) {
+//            System.out.println("Compares two configuration files and shows a difference.");
+//        }
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
