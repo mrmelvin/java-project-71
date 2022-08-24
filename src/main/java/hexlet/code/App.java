2aed807b029f66173dbe026050a498b23420861c
mr.melvin <korobov.mike@gmail.com>
2022-08-24 23:17:04 +0500

    Add Option -f

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,16 +5,15 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
+//final GLOBAL_DESCRIPTION = "Compares two configuration files and shows a difference.         filepath1         path to first file        filepath2         path to second file";
+@Command(name = "gendiff", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.\n\tfilepath1\tpath to first file\n\tfilepath2\tpath to second file")
 public class App implements Callable<Integer> {
 
-//    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-//    boolean usageHelpRequested;
-//
-//    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-//    boolean versionInfoRequested;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    File file;
 
     public Integer call() throws Exception {
 //        if (usageHelpRequested) {
